844f85ad4790e5daf033c3d5572eae1a209f4ace
Shubhangi Chaturvedi <chaturvedishubhangi26@gmail.com>
2018-10-30 11:58:59 +0530

    Gig.cs file added

---
+++ FullStackApplication/Models/Gig.cs
@@ -0,0 +1,14 @@
+using System;
+
+namespace FullStackPluralsight.Models
+{
+    public class Gig
+    {
+        public int Id { get; set; }
+        public ApplicationUser Artist { get; set; }
+        public DateTime DateTime { get; set; }
+        public string Venue { get; set; }
+        public Genre Genre { get; set; }
+    }
+
+}
